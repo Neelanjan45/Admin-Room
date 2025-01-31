8cf8841bb0e19af6a0cae15f8d0e143728e4bbcb
NilanjanAdak <niladak94@gmail.com>
2025-01-31 23:50:59 +0530

    Dashboard view added

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 import os
 from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 load_dotenv()
 
@@ -11,5 +12,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = r'sqlite:///' + os.getenv("DATABASE_URI"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 import routes
--- routes.py
+++ routes.py
@@ -1,9 +1,21 @@
-from app import app
-from flask import render_template
+from app import app, login_manager
+from flask import render_template, redirect, url_for
 from forms import LoginForm
+from flask_login import login_required, login_user
+from models import User
+from werkzeug.security import check_password_hash
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm(csrf_enabled=False)
     return render_template('login.html', form=form)
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
