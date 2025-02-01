109fd5882682894dafcfe3218c83511649babc59
NilanjanAdak <niladak94@gmail.com>
2025-02-01 06:59:33 +0530

    Login/Logout functionality implemented

--- routes.py
+++ routes.py
@@ -1,7 +1,7 @@
 from app import app, login_manager
 from flask import render_template, redirect, url_for
 from forms import LoginForm
-from flask_login import login_required, login_user
+from flask_login import login_required, login_user, logout_user
 from models import User
 from werkzeug.security import check_password_hash
 
@@ -13,9 +13,22 @@ def load_user(user_id):
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm(csrf_enabled=False)
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and check_password_hash(user.password, form.password.data):
+            login_user(user)
+            return redirect(url_for('dashboard', _external=True))
+        else:
+            return redirect(url_for('login'))
     return render_template('login.html', form=form)
 
 @app.route('/dashboard', methods=['GET', 'POST'])
 @login_required
 def dashboard():
     return render_template('dashboard.html')
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login', _external=True))
